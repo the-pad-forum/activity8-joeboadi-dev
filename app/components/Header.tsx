7d6d4d20ace31cfecc88690e8d4f6e5d56ecb9c9
Joe Boadi <joeboadi1@gmail.com>
2023-12-21 22:47:50 +0000

    Edit title name

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,17 +2,17 @@ import Image from 'next/image';
 import DP from '../media/profile-pic.jpg';
 import styles from '../styles/Header.module.css';
 
+
 const Header = () => {
   return (
-    <div className={styles.overlay}>
-        <header className={styles.header}>
+      <header className={styles.header}>
+        <div className={styles.overlay}>
           <Image className={styles.dp} src={DP} width="124" height="124" alt="" />
           <h1>Joe Boadi Orion</h1>
           <p>A Beginner Web Developer at The PAD Forum</p>
           {/* Social links go here */}
-        </header>
-    </div>
-   
+        </div>
+     </header>
   );
 };
 
