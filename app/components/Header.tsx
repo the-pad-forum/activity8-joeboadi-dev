42392a2215a203997689f66fe0cb53df5c2275e8
Joe Boadi <joeboadi1@gmail.com>
2023-12-21 16:56:40 +0000

    Setup tsconfig

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,10 +6,10 @@ const Header = () => {
   return (
     <div className={styles.overlay}>
         <header className={styles.header}>
-        <Image className={styles.dp} src={DP} width="124" height="124" alt="" />
-        <h1>Joe Boadi Orion</h1>
-        <p>A Beginner Web Developer at The PAD Forum</p>
-        {/* Social links go here */}
+          <Image className={styles.dp} src={DP} width="124" height="124" alt="" />
+          <h1>Joe Boadi Orion</h1>
+          <p>A Beginner Web Developer at The PAD Forum</p>
+          {/* Social links go here */}
         </header>
     </div>
    
