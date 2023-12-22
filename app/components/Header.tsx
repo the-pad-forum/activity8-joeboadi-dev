2385e9ce786687b247b535b3cceab777a03618e4
Joe Boadi <joeboadi1@gmail.com>
2023-12-22 03:15:10 +0000

    Update content

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,26 @@
 import Image from 'next/image';
 import DP from '../media/profile-pic.jpg';
 import styles from '../styles/Header.module.css';
+import { SocialIcon } from 'react-social-icons/component'
+import 'react-social-icons/facebook'
+import 'react-social-icons/x'
+import 'react-social-icons/linkedin'
+import 'react-social-icons/discord'
 
 const Header = () => {
   return (
-      <header className={styles.header}>
-        <div className={styles.overlay}>
-          <Image className={styles.dp} src={DP} width="124" height="124" alt="" />
-          <h1>Joe Boadi Orion</h1>
-          <p>A Beginner Web Developer at The PAD Forum</p>
+    <header className={styles.header}>
+      <div className={styles.overlay}>
+        <Image className={styles.dp} src={DP} width="124" height="124" alt="" />
+        <h1>Joe Boadi Orion</h1>
+        <p>A Beginner Web Developer at The PAD Forum</p>
           {/* Social links go here */}
-
-        </div>
-     </header>
+          <SocialIcon url="https://www.facebook.com/profile.php?id=100095549220353&mibextid=2JQ9oc" />
+          <SocialIcon url="https://x.com/joeboadiDev_?t=u2KiKAn8-pioz1Q80HYLgA&s=09" />
+          <SocialIcon url="https://www.linkedin.com/in/kwadjo-boadi-mantey-43b670271" />
+          <SocialIcon url="https://discord.com/users/1020877777706176563" />
+       </div>
+    </header>
   );
 };
 
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -2,9 +2,40 @@ import styles from '../styles/Skills.module.css';
 
 const Skills = () => {
   return (
-    <section className={styles.skills}>
+    <section className={styles.skillSection}>
       <h2>Skills</h2>
       {/* Skill contents go here */}
+      <div className={styles.skill}>
+			<span className={styles.skillName}>CSS</span>
+			<div className = {styles.progressBar} style={{width:'59%'}}>
+				<div className={styles.filled}></div>
+			</div>
+			<span className = {styles.skillPercent}>50%</span>
+		</div>
+
+    <div className={styles.skill}>
+			<span className={styles.skillName}>JS</span>
+			<div className = {styles.progressBar}>
+				<div className={styles.filled}></div>
+			</div>
+			<span className = {styles.skillPercent}>67%</span>
+		</div>
+
+    <div className={styles.skill}>
+			<span className={styles.skillName}>Python</span>
+			<div className = {styles.progressBar}>
+				<div className={styles.filled}></div>
+			</div>
+			<span className = {styles.skillPercent}>70%</span>
+		</div>
+
+    <div className={styles.skill}>
+			<span className={styles.skillName}>Communication</span>
+			<div className = {styles.progressBar}>
+				<div className={styles.filled}></div>
+			</div>
+			<span className = {styles.skillPercent}>50%</span>
+		</div>
     </section>
   );
 };
