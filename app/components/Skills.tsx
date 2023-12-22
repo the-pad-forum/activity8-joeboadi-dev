2a0adc6f94d1846cb37ae04055ed9ad43da86d21
Joe Boadi <joeboadi1@gmail.com>
2023-12-22 14:46:09 +0000

    Upgrade Skill with percentage fill

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -3,20 +3,20 @@ import styles from '../styles/Skills.module.css';
 const Skills = () => {
   return (
     <section className={styles.skillSection}>
-      <h2>Skills</h2>
+      <h2 className={styles.h2}>Skills</h2>
       {/* Skill contents go here */}
       <div className={styles.skill}>
 			<span className={styles.skillName}>CSS</span>
-			<div className = {styles.progressBar} style={{width:'59%'}}>
-				<div className={styles.filled}></div>
+			<div className = {styles.progressBar}>
+				<div className={styles.filled} style={{width:'50%'}}></div>
 			</div>
 			<span className = {styles.skillPercent}>50%</span>
 		</div>
 
     <div className={styles.skill}>
-			<span className={styles.skillName}>JS</span>
-			<div className = {styles.progressBar}>
-				<div className={styles.filled}></div>
+			<span className={styles.skillName}>Javascript</span>
+			<div className = {styles.progressBar} id=''>
+				<div className={styles.filled} style={{width:'67%'}}></div>
 			</div>
 			<span className = {styles.skillPercent}>67%</span>
 		</div>
@@ -24,7 +24,7 @@ const Skills = () => {
     <div className={styles.skill}>
 			<span className={styles.skillName}>Python</span>
 			<div className = {styles.progressBar}>
-				<div className={styles.filled}></div>
+				<div className={styles.filled} style={{width:'70%'}}></div>
 			</div>
 			<span className = {styles.skillPercent}>70%</span>
 		</div>
@@ -32,9 +32,9 @@ const Skills = () => {
     <div className={styles.skill}>
 			<span className={styles.skillName}>Communication</span>
 			<div className = {styles.progressBar}>
-				<div className={styles.filled}></div>
+				<div className={styles.filled} style={{width:'87%'}}></div>
 			</div>
-			<span className = {styles.skillPercent}>50%</span>
+			<span className = {styles.skillPercent}>87%</span>
 		</div>
     </section>
   );
