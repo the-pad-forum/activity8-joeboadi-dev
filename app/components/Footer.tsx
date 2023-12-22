9753a5a08e0036e99c5e5e3689fc0bd1a6f0f2fc
Joe Boadi <joeboadi1@gmail.com>
2023-12-22 14:44:13 +0000

    Modify Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,8 @@ import styles from '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      ©2023, The PAD Forum. All Rights Reserved.
+      ©2023, The PAD Forum. All Rights Reserved. <br />
+      <small>Made with NextJS</small>
     </footer>
   );
 };
